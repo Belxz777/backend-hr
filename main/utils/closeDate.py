b897ba5347b0b8f0107ee3ae0827308097d2fc01
Roman <140193303+Belxz777@users.noreply.github.com>
2025-03-25 21:01:31 +0400

    fixes

--- main/utils/closeDate.py
+++ main/utils/closeDate.py
@@ -1,44 +1,27 @@
 from datetime import datetime, timedelta
+from django.utils.timezone import make_aware
 
 def calculate_close_date(hours_needed, current_time):
-    """
-    Функция для вычисления даты и времени завершения задачи.
+    current_time = make_aware(current_time)  # Делаем время осведомленным о временной зоне
     
-    :param hours_needed: Количество часов, необходимых для выполнения задачи.
-    :param current_time: Текущее время в формате datetime.
-    :return: Дата и время завершения задачи в формате datetime.
-    """
-    
-    # Определяем начало и конец рабочего дня
     work_start = current_time.replace(hour=8, minute=0, second=0, microsecond=0)
     work_end = current_time.replace(hour=17, minute=0, second=0, microsecond=0)
-
-    # Если текущее время до начала рабочего дня, устанавливаем его на начало рабочего дня
+    
     if current_time < work_start:
         current_time = work_start
-    
-    # Если текущее время после конца рабочего дня, переходим на следующий рабочий день
     elif current_time > work_end:
-        current_time += timedelta(days=1)
-        current_time = current_time.replace(hour=8, minute=0, second=0, microsecond=0)
-
-    # Подсчитываем оставшиеся часы
+        current_time = (current_time + timedelta(days=1)).replace(hour=8, minute=0, second=0, microsecond=0)
+    
     while hours_needed > 0:
-        # Проверяем, сколько времени осталось в текущем рабочем дне
         remaining_today = (work_end - current_time).seconds / 3600
         
         if remaining_today >= hours_needed:
-            # Если оставшегося времени достаточно для выполнения задачи
-            close_date = current_time + timedelta(hours=hours_needed)
-            return close_date
+            return current_time + timedelta(hours=hours_needed)
         
-        # Если оставшегося времени недостаточно, вычитаем его и переходим к следующему дню
         hours_needed -= remaining_today
-        current_time = work_end + timedelta(days=1)  # Переход на следующий рабочий день
-        current_time = current_time.replace(hour=8, minute=0, second=0, microsecond=0)
-
+        current_time = (work_end + timedelta(days=1)).replace(hour=8, minute=0, second=0, microsecond=0)
+    
     return current_time
-
 # Пример использования функции
 # current_time = datetime.now()  # Текущее время
 # hours_needed = 10  # Необходимое количество часов для выполнения задачи
