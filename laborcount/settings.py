2ea581e0b80dfc6527e3642e1fb125a59e6f250d
Belxz777 <belxz888@gmail.com>
2025-07-21 08:51:31 +0400

    now

--- laborcount/settings.py
+++ laborcount/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
