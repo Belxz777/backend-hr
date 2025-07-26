127b60428a9938d9762de8647e8de140479c43be
Belxz777 <belxz888@gmail.com>
2025-07-26 16:21:22 +0400

    apply

--- laborcount/urls.py
+++ laborcount/urls.py
@@ -1,11 +1,8 @@
-
-from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 
 urlpatterns = [
     path('api/v1/',include('main.urls')),
-    # path('admin/', admin.site.urls),
     path('', lambda request: JsonResponse({'status': 'Server is running'})),
     
 ]
--- main/apps.py
+++ main/apps.py
@@ -1,12 +1,4 @@
-import logging
 from django.apps import AppConfig
-
-from .log_handler import endpoint_handler 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-    def ready(self):
-        import main.signals
-        logger = logging.getLogger('app')
-        logger.addHandler(endpoint_handler)
-        logger.setLevel(logging.INFO)
